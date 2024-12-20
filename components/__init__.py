4c3b307bbf760e3bf5956a6790a51b1c5a0aa2a6
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-12-20 10:45:12 +0100

    Update __init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -1,1 +1,18 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import (CONF_ID)
 
+shelly3empro_ns = cg.esphome_ns.namespace("shelly3empro")
+Shelly3EMPRO = shelly3empro_ns.class_("Shelly3EMPRO", cg.PollingComponent)
+
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(Shelly3EMPRO),
+               
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+   var = cg.new_Pvariable(config[CONF_ID])
+   await cg.register_component(var, config)
